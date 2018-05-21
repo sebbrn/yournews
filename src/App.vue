c5081548fe82796b1e40f5eecc266671dcef6e7e
Sebastian Brune <s.brun@gmx.de>
2018-05-21 13:52:40 +0200

    Initiated Vue-App

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <NewsFeed/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NewsFeed from "./components/NewsFeed";
+
+export default {
+  name: 'app',
+  components: {
+      NewsFeed,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NewsFeed.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Your personal newsfeed</h1>
+</template>
+
+<script>
+    export default {
+        name: "NewsFeed"
+    }
+</script>
+
+<style scoped>
+
+</style>
