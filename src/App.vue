fbcf7cc0cd149ec87bd58f9222d8b7281d85b310
Sebastian Brune <s.brun@gmx.de>
2018-05-21 14:41:44 +0200

    NewsFeed with SportBible example

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <NewsFeed/>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <NewsFeed/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import NewsFeed from "./components/NewsFeed";
+    import HelloWorld from './components/HelloWorld.vue'
+    import NewsFeed from "./components/NewsFeed";
 
-export default {
-  name: 'app',
-  components: {
-      NewsFeed,
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            NewsFeed,
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    img {
+        width: 10rem;
+    }
 </style>
--- src/components/NewsFeed.vue
+++ src/components/NewsFeed.vue
@@ -1,10 +1,40 @@
 <template>
-    <h1>Your personal newsfeed</h1>
+    <div>
+        <ul v-if="posts && posts.length">
+            <li v-for="post of posts">
+                <a :href=post.url><strong>{{post.title}}</strong></a>
+                <p>{{post.description}}</p>
+                <img :src=post.urlToImage>
+            </li>
+        </ul>
+        <ul v-if="errors && errors.length">
+            <li v-for="error of errors">
+                {{error.message}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: "NewsFeed"
+        name: "NewsFeed",
+        data() {
+            return {
+                posts: []
+            }
+        },
+        // when the component is created
+        created() {
+            axios.get('https://newsapi.org/v2/top-headlines?sources=the-sport-bible&apiKey=fe0c5b3aa3864125b9ef31e528b8b2e4')
+                .then(response => {
+                    this.posts = response.data.articles;
+                })
+                .catch(e => {
+                    this.errors.push(e);
+                })
+        }
     }
 </script>
 
