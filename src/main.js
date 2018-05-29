aec90218c3ac437e2a8a44791d2e5ab228f1c34f
Sebastian Brune <s.brun@gmx.de>
2018-05-29 21:07:51 +0200

    added semicolon

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 
-Vue.use(VueMaterial)
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
 
